e28575cce84e08ef61452323a77b4c4d0790e0a7
Brian <tiu.brian@byu.edu>
2016-01-26 21:33:03 -0700

    game model changes - brian

--- Catan/src/model/GameModel.java
+++ Catan/src/model/GameModel.java
@@ -18,11 +18,19 @@ import shared.locations.VertexLocation;
 public class GameModel {
 	private Map map;
 	private Bank bank;
+	private ArrayList<Player> players;
+	private int playerIndex;
 
 // Constructor
-	public GameModel(Map map) {
+	public GameModel(Map map, int playerIndex) {
 		this.map = map;
 		this.bank = Bank.BANK;
+		this.playerIndex = playerIndex;
+
+		players.add(new Player(0, CatanColor.RED , "Player1"));
+		players.add(new Player(1, CatanColor.GREEN, "Player2"));
+		players.add(new Player(2, CatanColor.ORANGE, "Player3"));
+		players.add(new Player(3, CatanColor.BLUE, "Player4"));
 	}
 
 // Public methods
@@ -31,7 +39,7 @@ public class GameModel {
 	 *
 	 * @return
      */
-	public boolean sendChat() {
+	public boolean sendChat(String message) {
 		return true;
 	}
 
@@ -43,21 +51,91 @@ public class GameModel {
 		return false;
 	}
 
-	public boolean rollNumber() {
+	public boolean rollNumber(int number) {
+
 		return false;
 	}
 
-	public boolean buildRoad() {
+	/**
+	 * Returns a boolean whether or not the player can build a road at the given location
+	 * @param free
+	 * @param loc
+     * @return
+     */
+	public boolean buildRoad(boolean free, EdgeLocation loc) {
+		boolean canBuild;
+		if(map.canBuildRoad(free, loc)) {
+			canBuild = true;
+		} else {
+			canBuild = false;
+		}
 
+		if(!canBuild) {
+			return false;
+		}
+
+		if(free) {
+			if (!players.get(playerIndex).canBuildRoad(loc)) {
+				return false;
+			}
+		}
+
+		return true;
 	}
 
-	public boolean buildSettlement() {
-		return false;
+	/**
+	 * Returns a boolean whether or not the player can build a road at the given location
+	 * @param free
+	 * @param loc
+     * @return
+     */
+	public boolean buildSettlement(boolean free, VertexLocation loc) {
+
+		boolean canBuild;
+		if(map.canBuildSettlement(free, loc)) {
+			canBuild = true;
+		} else {
+			canBuild = false;
+		}
+
+		if(!canBuild) {
+			return false;
+		}
+
+		if(free) {
+			if (!players.get(playerIndex).canBuildSettlement(loc)) {
+				return false;
+			}
+		}
+
+		return true;
 	}
 
-	public boolean buildCity() {
-		return false;
-	}
+	/**
+	 * Returns a boolean whether or not the player can build a city at the given location
+	 * @param free
+	 * @param loc
+     * @return
+     */
+	public boolean buildCity(boolean free, VertexLocation loc) {
+		boolean canBuild;
+		if(map.canBuildCity(free, loc)) {
+			canBuild = true;
+		} else {
+			canBuild = false;
+		}
+
+		if(!canBuild) {
+			return false;
+		}
+
+		if(free) {
+			if (!players.get(playerIndex).canBuildCity(loc)) {
+				return false;
+			}
+		}
+
+		return true;	}
 
 	public boolean offerTrade() {
 		return false;
@@ -87,16 +165,31 @@ public class GameModel {
 		return false;
 	}
 
+
 	public boolean roadBuilding() {
-		return false;
+
 	}
 
 	public boolean monopoly() {
 		return false;
 	}
 
-	public boolean monument() {
-		return false;
+	public boolean monument(int playerID) {
+		ArrayList<DevelopmentCard> cards = players.get(playerID).getPlayerHand().getDevelopmentCards();
+		int count = 0;
+
+		for(DevelopmentCard dc : cards) {
+			if(dc.getType() == DevCardType.MONUMENT) {
+				count++;
+			}
+		}
+		int difference = 10 - count;
+
+		if(difference >= players.get(playerID).getVictoryPoints()) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 // Private methods
