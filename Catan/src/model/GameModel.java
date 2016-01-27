f4402150069bfa7c0383091356d04ba5d38d71ef
Brian <tiu.brian@byu.edu>
2016-01-26 20:59:30 -0700

    game model changes - brian

--- Catan/src/model/GameModel.java
+++ Catan/src/model/GameModel.java
@@ -16,5 +16,88 @@ import shared.locations.HexLocation;
 import shared.locations.VertexLocation;
 
 public class GameModel {
+	private Map map;
+	private Bank bank;
 
+// Constructor
+	public GameModel(Map map) {
+		this.map = map;
+		this.bank = Bank.BANK;
+	}
+
+// Public methods
+
+	/**
+	 *
+	 * @return
+     */
+	public boolean sendChat() {
+		return true;
+	}
+
+	public boolean acceptTrade() {
+		return false;
+	}
+
+	public boolean discardCards() {
+		return false;
+	}
+
+	public boolean rollNumber() {
+		return false;
+	}
+
+	public boolean buildRoad() {
+
+	}
+
+	public boolean buildSettlement() {
+		return false;
+	}
+
+	public boolean buildCity() {
+		return false;
+	}
+
+	public boolean offerTrade() {
+		return false;
+	}
+
+	public boolean maritimeTrade() {
+		return false;
+	}
+
+	public boolean robPlayer() {
+		return false;
+	}
+
+	public boolean finishTurn() {
+		return false;
+	}
+
+	public boolean buyDevCard() {
+		return false;
+	}
+
+	public boolean soldier() {
+		return false;
+	}
+
+	public boolean yearOfPlenty() {
+		return false;
+	}
+
+	public boolean roadBuilding() {
+		return false;
+	}
+
+	public boolean monopoly() {
+		return false;
+	}
+
+	public boolean monument() {
+		return false;
+	}
+
+// Private methods
 }
