f5f0026b3d37e969e943b485e7a5caa66ae52430
hunterrees <hunterrees13@gmail.com>
2016-01-28 20:41:35 -0700

    I created all of the translator classes (other than the big one for the model).

---
+++ Catan/src/translators/ModelTranslator.java
@@ -0,0 +1,15 @@
+package translators;
+
+import model.GameModel;
+
+/**
+ * This class should parse through the model that is returned
+ */
+public class ModelTranslator {
+	
+	public ModelTranslator(){
+	
+	};
+	
+	public GameModel getModelfromJSON(int versionID){return null;};
+}
